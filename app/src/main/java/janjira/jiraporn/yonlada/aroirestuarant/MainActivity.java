189eb0e30acbdf469be75feb0cbac0ceeca15b8a
MasterUNG <phrombutr@gmail.com>
2017-11-30 12:34:03 +0700

    Hamberger OK

--- app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/MainActivity.java
+++ app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/MainActivity.java
@@ -1,11 +1,21 @@
 package janjira.jiraporn.yonlada.aroirestuarant;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import janjira.jiraporn.yonlada.aroirestuarant.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
 
 
     @Override
@@ -19,6 +29,44 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.contentFragmentMain,new MainFragment()).commit();
         }
 
+//        Cteate Toolbar
+        cteateToolbar();
+
     } //Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void cteateToolbar() {
+        toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
+                drawerLayout, R.string.open, R.string.close);
+
+    }
 
 } // Main Class
