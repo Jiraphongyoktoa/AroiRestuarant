d78b806204c495b9ab04b761fc95f097e8b44bb4
MasterUNG <phrombutr@gmail.com>
2017-12-22 12:50:57 +0700

    Choose Item OK

--- app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/SerciveOrderActivity.java
+++ app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/SerciveOrderActivity.java
@@ -1,8 +1,11 @@
 package janjira.jiraporn.yonlada.aroirestuarant;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,16 +25,25 @@ public class SerciveOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sercive_order);
 
 //        Get Value Frome Intent
-        nameFoodString = getIntent().getStringExtra("NameFood");
-        categoryString = getIntent().getStringExtra("Category");
-        imagePathString = getIntent().getStringExtra("ImagePath");
-        priceString = getIntent().getStringExtra("Price");
-        detailString = getIntent().getStringExtra("Detail");
+        getValueFromeIntent();
 
 //        Create Toolbar
         createToolbar();
 
 //        Add Fragment
+        addFragment(savedInstanceState);
+
+    }   // Main Method
+
+    private void getValueFromeIntent() {
+        nameFoodString = getIntent().getStringExtra("NameFood");
+        categoryString = getIntent().getStringExtra("Category");
+        imagePathString = getIntent().getStringExtra("ImagePath");
+        priceString = getIntent().getStringExtra("Price");
+        detailString = getIntent().getStringExtra("Detail");
+    }
+
+    private void addFragment(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contenfragmentService, DetailFragment.detailInstance(
@@ -40,8 +52,7 @@ public class SerciveOrderActivity extends AppCompatActivity {
                     .addToBackStack(null)
                     .commit();
         }
-
-    }   // Main Method
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,6 +63,12 @@ public class SerciveOrderActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+        if (item.getItemId() == R.id.itemMenuOrder) {
+
+            chooseItemOrder();
+
+        }
+
         if (item.getItemId() == R.id.itemMenuNewRegister) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.contenfragmentService, new RegisterFragment())
@@ -60,6 +77,28 @@ public class SerciveOrderActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void chooseItemOrder() {
+
+        final int[] itemAInt = {1};
+
+        CharSequence[] charSequences = new CharSequence[]{"1 Set", "2 Set", "3 Set", "4 Set", "5 Set"};
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(SerciveOrderActivity.this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_fruit);
+        builder.setTitle("Please Choose Item");
+        builder.setSingleChoiceItems(charSequences, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                itemAInt[0] = itemAInt[0] + i;
+                Log.d("22DecV1", "Item ==> " + itemAInt[0]);
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
     }
 
     private void createToolbar() {
